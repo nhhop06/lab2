ac286b075618f0ca4bf3559aa8f944ec3a7381c5
nhhop <nhhop06@gamil.com>
2022-08-08 12:57:30 +0700

    c

---
+++ main.js
@@ -0,0 +1,472 @@
+//Khai báo biến
+var fullName = "Ngô Hữu Hợp made this website";
+var age = "Bật console dể tán gái =))))" ;
+
+//Gọi hàm thông báo
+alert(fullName);
+alert(age);
+ 
+
+setInterval(function () {
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 0);
+    setTimeout(function () {
+      console.count(" Anh Yêu Em");
+    }, 50);
+    setTimeout(function () {
+      console.count("  Anh Yêu Em");
+    }, 100);
+    setTimeout(function () {
+      console.count("   Anh Yêu Em");
+    }, 150);
+    setTimeout(function () {
+      console.count("    Anh Yêu Em");
+    }, 200);
+    setTimeout(function () {
+      console.count("     Anh Yêu Em");
+    }, 250);
+    setTimeout(function () {
+      console.count("      Anh Yêu Em");
+    }, 300);
+    setTimeout(function () {
+      console.count("       Anh Yêu Em");
+    }, 350);
+    setTimeout(function () {
+      console.count("        Anh Yêu Em");
+    }, 400);
+    setTimeout(function () {
+      console.count("         Anh Yêu Em");
+    }, 450);
+    setTimeout(function () {
+      console.count("          Anh Yêu Em");
+    }, 500);
+    setTimeout(function () {
+      console.count("           Anh Yêu Em");
+    }, 550);
+    setTimeout(function () {
+      console.count("            Anh Yêu Em");
+    }, 600);
+    setTimeout(function () {
+      console.count("             Anh Yêu Em");
+    }, 650);
+    setTimeout(function () {
+      console.count("              Anh Yêu Em");
+    }, 700);
+    setTimeout(function () {
+      console.count("               Anh Yêu Em");
+    }, 750);
+    setTimeout(function () {
+      console.count("                Anh Yêu Em");
+    }, 800);
+    setTimeout(function () {
+      console.count("                 Anh Yêu Em");
+    }, 850);
+    setTimeout(function () {
+      console.count("                  Anh Yêu Em");
+    }, 900);
+    setTimeout(function () {
+      console.count("                 Anh Yêu Em");
+    }, 950);
+    setTimeout(function () {
+      console.count("                Anh Yêu Em");
+    }, 1000);
+    setTimeout(function () {
+      console.count("               Anh Yêu Em");
+    }, 1050);
+    setTimeout(function () {
+      console.count("              Anh Yêu Em");
+    }, 1100);
+    setTimeout(function () {
+      console.count("             Anh Yêu Em");
+    }, 1150);
+    setTimeout(function () {
+      console.count("            Anh Yêu Em");
+    }, 1200);
+    setTimeout(function () {
+      console.count("           Anh Yêu Em");
+    }, 1250);
+    setTimeout(function () {
+      console.count("          Anh Yêu Em");
+    }, 1300);
+    setTimeout(function () {
+      console.count("         Anh Yêu Em");
+    }, 1350);
+    setTimeout(function () {
+      console.count("        Anh Yêu Em");
+    }, 1400);
+    setTimeout(function () {
+      console.count("       Anh Yêu Em");
+    }, 1450);
+    setTimeout(function () {
+      console.count("      Anh Yêu Em");
+    }, 1500);
+    setTimeout(function () {
+      console.count("     Anh Yêu Em");
+    }, 1550);
+    setTimeout(function () {
+      console.count("    Anh Yêu Em");
+    }, 1600);
+    setTimeout(function () {
+      console.count("   Anh Yêu Em");
+    }, 1650);
+    setTimeout(function () {
+      console.count("  Anh Yêu Em");
+    }, 1700);
+    setTimeout(function () {
+      console.count(" Anh Yêu Em");
+    }, 1750);
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 1800);
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 1850);
+    setTimeout(function () {
+      console.count(" Anh Yêu Em");
+    }, 1900);
+    setTimeout(function () {
+      console.count("  Anh Yêu Em");
+    }, 1950);
+    setTimeout(function () {
+      console.count("   Anh Yêu Em");
+    }, 2000);
+    setTimeout(function () {
+      console.count("    Anh Yêu Em");
+    }, 2050);
+    setTimeout(function () {
+      console.count("     Anh Yêu Em");
+    }, 2100);
+    setTimeout(function () {
+      console.count("      Anh Yêu Em");
+    }, 2150);
+    setTimeout(function () {
+      console.count("       Anh Yêu Em");
+    }, 2200);
+    setTimeout(function () {
+      console.count("        Anh Yêu Em");
+    }, 2250);
+    setTimeout(function () {
+      console.count("         Anh Yêu Em");
+    }, 2300);
+    setTimeout(function () {
+      console.count("          Anh Yêu Em");
+    }, 2350);
+    setTimeout(function () {
+      console.count("           Anh Yêu Em");
+    }, 2400);
+    setTimeout(function () {
+      console.count("            Anh Yêu Em");
+    }, 2450);
+    setTimeout(function () {
+      console.count("             Anh Yêu Em");
+    }, 2500);
+    setTimeout(function () {
+      console.count("              Anh Yêu Em");
+    }, 2550);
+    setTimeout(function () {
+      console.count("               Anh Yêu Em");
+    }, 2600);
+    setTimeout(function () {
+      console.count("                Anh Yêu Em");
+    }, 2650);
+    setTimeout(function () {
+      console.count("                 Anh Yêu Em");
+    }, 2700);
+    setTimeout(function () {
+      console.count("                  Anh Yêu Em");
+    }, 2750);
+    setTimeout(function () {
+      console.count("                 Anh Yêu Em");
+    }, 2800);
+    setTimeout(function () {
+      console.count("                Anh Yêu Em");
+    }, 2850);
+    setTimeout(function () {
+      console.count("               Anh Yêu Em");
+    }, 2900);
+    setTimeout(function () {
+      console.count("              Anh Yêu Em");
+    }, 2950);
+    setTimeout(function () {
+      console.count("             Anh Yêu Em");
+    }, 3000);
+    setTimeout(function () {
+      console.count("            Anh Yêu Em");
+    }, 3050);
+    setTimeout(function () {
+      console.count("           Anh Yêu Em");
+    }, 3100);
+    setTimeout(function () {
+      console.count("          Anh Yêu Em");
+    }, 3150);
+    setTimeout(function () {
+      console.count("         Anh Yêu Em");
+    }, 3200);
+    setTimeout(function () {
+      console.count("        Anh Yêu Em");
+    }, 3250);
+    setTimeout(function () {
+      console.count("       Anh Yêu Em");
+    }, 3300);
+    setTimeout(function () {
+      console.count("      Anh Yêu Em");
+    }, 3350);
+    setTimeout(function () {
+      console.count("     Anh Yêu Em");
+    }, 3400);
+    setTimeout(function () {
+      console.count("    Anh Yêu Em");
+    }, 3450);
+    setTimeout(function () {
+      console.count("   Anh Yêu Em");
+    }, 3500);
+    setTimeout(function () {
+      console.count("  Anh Yêu Em");
+    }, 3550);
+    setTimeout(function () {
+      console.count(" Anh Yêu Em");
+    }, 3600);
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 3650);
+    setTimeout(function () {
+      console.count("Anh Yêu  Em");
+    }, 3700);
+    setTimeout(function () {
+      console.count("Anh Yêu   Em");
+    }, 3750);
+    setTimeout(function () {
+      console.count("Anh Yêu    Em");
+    }, 3800);
+    setTimeout(function () {
+      console.count("Anh Yêu    Em");
+    }, 3850);
+    setTimeout(function () {
+      console.count("Anh Yêu     Em");
+    }, 3900);
+    setTimeout(function () {
+      console.count("Anh Yêu      Em");
+    }, 3950);
+    setTimeout(function () {
+      console.count("Anh Yêu       Em");
+    }, 4000);
+    setTimeout(function () {
+      console.count("Anh Yêu        Em");
+    }, 4050);
+    setTimeout(function () {
+      console.count("Anh Yêu         Em");
+    }, 4100);
+    setTimeout(function () {
+      console.count("Anh Yêu          Em");
+    }, 4150);
+    setTimeout(function () {
+      console.count("Anh Yêu           Em");
+    }, 4200);
+    setTimeout(function () {
+      console.count("Anh Yêu            Em");
+    }, 4250);
+    setTimeout(function () {
+      console.count("Anh Yêu             Em");
+    }, 4300);
+    setTimeout(function () {
+      console.count("Anh Yêu              Em");
+    }, 4350);
+    setTimeout(function () {
+      console.count("Anh Yêu               Em");
+    }, 4400);
+    setTimeout(function () {
+      console.count("Anh Yêu                Em");
+    }, 4450);
+    setTimeout(function () {
+      console.count("Anh Yêu                 Em");
+    }, 4500);
+    setTimeout(function () {
+      console.count("Anh Yêu                  Em");
+    }, 4550);
+    setTimeout(function () {
+      console.count("Anh Yêu                   Em");
+    }, 4600);
+    setTimeout(function () {
+      console.count("Anh  Yêu                  Em");
+    }, 4650);
+    setTimeout(function () {
+      console.count("Anh   Yêu                 Em");
+    }, 4700);
+    setTimeout(function () {
+      console.count("Anh    Yêu                Em");
+    }, 4750);
+    setTimeout(function () {
+      console.count("Anh     Yêu               Em");
+    }, 4800);
+    setTimeout(function () {
+      console.count("Anh      Yêu              Em");
+    }, 4850);
+    setTimeout(function () {
+      console.count("Anh       Yêu             Em");
+    }, 4900);
+    setTimeout(function () {
+      console.count("Anh        Yêu            Em");
+    }, 4950);
+    setTimeout(function () {
+      console.count("Anh         Yêu           Em");
+    }, 5000);
+    setTimeout(function () {
+      console.count("Anh          Yêu          Em");
+    }, 5050);
+    setTimeout(function () {
+      console.count("Anh           Yêu         Em");
+    }, 5100);
+    setTimeout(function () {
+      console.count("Anh            Yêu        Em");
+    }, 5150);
+    setTimeout(function () {
+      console.count("Anh             Yêu       Em");
+    }, 5200);
+    setTimeout(function () {
+      console.count("Anh              Yêu      Em");
+    }, 5250);
+    setTimeout(function () {
+      console.count("Anh               Yêu     Em");
+    }, 5300);
+    setTimeout(function () {
+      console.count("Anh                Yêu    Em");
+    }, 5350);
+    setTimeout(function () {
+      console.count("Anh                 Yêu   Em");
+    }, 5400);
+    setTimeout(function () {
+      console.count("Anh                  Yêu  Em");
+    }, 5450);
+    setTimeout(function () {
+      console.count("Anh                   Yêu Em");
+    }, 5500);
+    setTimeout(function () {
+      console.count("Anh                   Yêu Em");
+    }, 5550);
+    setTimeout(function () {
+      console.count("Anh                   Yêu Em");
+    }, 5600);
+    setTimeout(function () {
+      console.count("Anh                   Yêu Em");
+    }, 5650);
+    setTimeout(function () {
+      console.count(" Anh                  Yêu Em");
+    }, 5700);
+    setTimeout(function () {
+      console.count("  Anh                 Yêu Em");
+    }, 5750);
+    setTimeout(function () {
+      console.count("   Anh                Yêu Em");
+    }, 5800);
+    setTimeout(function () {
+      console.count("    Anh               Yêu Em");
+    }, 5850);
+    setTimeout(function () {
+      console.count("     Anh              Yêu Em");
+    }, 5900);
+    setTimeout(function () {
+      console.count("      Anh             Yêu Em");
+    }, 5950);
+    setTimeout(function () {
+      console.count("       Anh            Yêu Em");
+    }, 6000);
+    setTimeout(function () {
+      console.count("        Anh           Yêu Em");
+    }, 6050);
+    setTimeout(function () {
+      console.count("         Anh          Yêu Em");
+    }, 6100);
+    setTimeout(function () {
+      console.count("          Anh         Yêu Em");
+    }, 6150);
+    setTimeout(function () {
+      console.count("           Anh        Yêu Em");
+    }, 6200);
+    setTimeout(function () {
+      console.count("            Anh       Yêu Em");
+    }, 6250);
+    setTimeout(function () {
+      console.count("             Anh      Yêu Em");
+    }, 6300);
+    setTimeout(function () {
+      console.count("              Anh     Yêu Em");
+    }, 6350);
+    setTimeout(function () {
+      console.count("               Anh    Yêu Em");
+    }, 6400);
+    setTimeout(function () {
+      console.count("                Anh   Yêu Em");
+    }, 6450);
+    setTimeout(function () {
+      console.count("                 Anh  Yêu Em");
+    }, 6500);
+    setTimeout(function () {
+      console.count("                  Anh Yêu Em");
+    }, 6550);
+    setTimeout(function () {
+      console.count("                  Anh Yêu Em");
+    }, 6600);
+    setTimeout(function () {
+      console.count("                  Anh Yêu Em");
+    }, 6650);
+    setTimeout(function () {
+      console.count("                 Anh Yêu Em");
+    }, 6700);
+    setTimeout(function () {
+      console.count("                Anh Yêu Em");
+    }, 6750);
+    setTimeout(function () {
+      console.count("               Anh Yêu Em");
+    }, 6800);
+    setTimeout(function () {
+      console.count("              Anh Yêu Em");
+    }, 6850);
+    setTimeout(function () {
+      console.count("             Anh Yêu Em");
+    }, 6900);
+    setTimeout(function () {
+      console.count("            Anh Yêu Em");
+    }, 6950);
+    setTimeout(function () {
+      console.count("           Anh Yêu Em");
+    }, 7000);
+    setTimeout(function () {
+      console.count("          Anh Yêu Em");
+    }, 7050);
+    setTimeout(function () {
+      console.count("         Anh Yêu Em");
+    }, 7100);
+    setTimeout(function () {
+      console.count("        Anh Yêu Em");
+    }, 7150);
+    setTimeout(function () {
+      console.count("       Anh Yêu Em");
+    }, 7200);
+    setTimeout(function () {
+      console.count("      Anh Yêu Em");
+    }, 7250);
+    setTimeout(function () {
+      console.count("     Anh Yêu Em");
+    }, 7300);
+    setTimeout(function () {
+      console.count("    Anh Yêu Em");
+    }, 7350);
+    setTimeout(function () {
+      console.count("   Anh Yêu Em");
+    }, 7400);
+    setTimeout(function () {
+      console.count("  Anh Yêu Em");
+    }, 7450);
+    setTimeout(function () {
+      console.count(" Anh Yêu Em");
+    }, 7500);
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 7550);
+    setTimeout(function () {
+      console.count("Anh Yêu Em");
+    }, 7600);
+    
+}, 7600)
+
